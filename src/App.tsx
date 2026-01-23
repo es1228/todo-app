486c880e2f9385fb74acc6fca3bc371ba16aa555
Evan Sidhu <129010227+es1228@users.noreply.github.com>
2026-01-23 16:03:12 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+//import { useState } from 'react'
+import './App.css'
+import Label from './Label';
+
+// main app
+export default function App() {
+  // components
+  return (
+    <>
+      <Label/>
+    </>
+  );
+}
---
+++ src/Label.tsx
@@ -0,0 +1,12 @@
+//import React from "react";
+
+// label component
+export default function Label() {
+    return (
+        <>
+            <div className="myLabel">
+                <p className="text-lg italic">This is a Label</p>
+            </div>
+        </>
+    )
+}
